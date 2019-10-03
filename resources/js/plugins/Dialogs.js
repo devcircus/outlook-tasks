6f9bd6dc39aae13f43fe8be10ac0724cde877037
Clayton Stone <devcircus@outlook.com>
2019-10-03 12:44:02 -0500

    Show confirm dialog when deleting email. Add delete all option to email table.

--- resources/js/plugins/Dialogs.js
+++ resources/js/plugins/Dialogs.js
@@ -1,9 +1,9 @@
 export default {
     install (Vue, options) {
-        Vue.prototype.$showDialog = (level, subject, verb, callback) => {
+        Vue.prototype.$showDialog = (level, subject, verb, callback, multiple = false) => {
             Vue.prototype.$modal.show('dialogModal', {
                 title: level === 'warning' ? 'Caution!' : 'Info',
-                text: `Are you sure you want to ${verb} this ${subject}?`,
+                text: multiple ? `Are you sure you want to ${verb} all of these ${subject}?` : `Are you sure you want to ${verb} this ${subject}?`,
                 buttons: [
                     {
                         title: verb.charAt(0).toUpperCase() + verb.slice(1) + ' ' + subject.charAt(0).toUpperCase() + subject.slice(1),
