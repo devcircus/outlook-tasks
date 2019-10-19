2690750561bcb048aeb4c26187a30f570537805d
Clayton Stone <devcircus@outlook.com>
2019-10-19 10:33:48 -0500

    Replace hard-coded values with env vars.

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,4 +1,34 @@
+import { config } from 'Config';
+
 export default {
+    meta: {
+        admin: {
+            name: config.admin.name,
+            email: config.admin.email,
+            sites: {
+                github: {
+                    profile: config.admin.sites.github.profile,
+                    url: config.admin.sites.github.url,
+                },
+            },
+        },
+        links: {
+            template: {
+                name: config.links.template.name,
+                url: config.links.template.url,
+            },
+            local: {
+                dashboard: {
+                    name: config.links.local.dashboard.name,
+                    url: config.links.local.dashboard.url,
+                },
+                corporate: {
+                    name: config.links.local.corporate.name,
+                    url: config.links.local.corporate.url,
+                },
+            },
+        },
+    },
     activeCategory: 'all',
     activeCalendar: 'all',
     quantities: {
